eeae31ba5ddccde5a1aa11a122db6e8d8a49cd8f
Eric Thorsen <ethorsen@thortech-solutions.com>
2010-08-22 18:12:39 -0400

    Fixed licenses in files to all be EPL

--- org-enclojure-repl-client/src/main/resources/org/enclojure/ide/resources/icon_maps.clj
+++ org-enclojure-repl-client/src/main/resources/org/enclojure/ide/resources/icon_maps.clj
@@ -1,18 +1,14 @@
 (comment
-;*******************************************************************************
+;*
 ;*    Copyright (c) ThorTech, L.L.C.. All rights reserved.
 ;*    The use and distribution terms for this software are covered by the
-;*    GNU General Public License, version 2
-;*    (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) with classpath
-;*    exception (http://www.gnu.org/software/classpath/license.html)
-;*    which can be found in the file GPL-2.0+ClasspathException.txt at the root
-;*    of this distribution.
+;*    Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;*    which can be found in the file epl-v10.html at the root of this distribution.
 ;*    By using this software in any fashion, you are agreeing to be bound by
 ;*    the terms of this license.
 ;*    You must not remove this notice, or any other, from this software.
-;*******************************************************************************
+;*
 ;*    Author: Eric Thorsen
-;*******************************************************************************
 )
 (ns org.enclojure.ide.resources.icon-maps)
 
